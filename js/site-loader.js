fbd9b9253da17f2a3f0b6ebca7b3e21ab677f92f
toniopaceict <toniopaceict@gmail.com>
2026-03-25 14:35:57 +0100

    Update site-loader.js

--- js/site-loader.js
+++ js/site-loader.js
@@ -1,11 +1,19 @@
 (function () {
-  const MENU_VERSION = "1";
+  /* =======================================================
+     CHANGE THIS NUMBER WHEN YOU UPDATE THE MENU
+     Example:
+     If you edit menu.css or header.js, change 7 to 8
+     Then students only need a normal refresh
+     ======================================================= */
+  const MENU_VERSION = "7";
 
+  /* Load shared menu CSS */
   const css = document.createElement("link");
   css.rel = "stylesheet";
   css.href = "https://toniopaceict.github.io/level4_css/css/menu.css?v=" + MENU_VERSION;
   document.head.appendChild(css);
 
+  /* Load shared header/menu JS */
   const headerScript = document.createElement("script");
   headerScript.src = "https://toniopaceict.github.io/level4_css/js/header.js?v=" + MENU_VERSION;
   document.body.appendChild(headerScript);
