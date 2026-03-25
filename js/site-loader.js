f167d81075d1553e1a7a020cad31418246b1294b
toniopaceict <toniopaceict@gmail.com>
2026-03-25 14:43:23 +0100

    Update site-loader.js

--- js/site-loader.js
+++ js/site-loader.js
@@ -1,20 +1,37 @@
 (function () {
   /* =======================================================
-     CHANGE THIS NUMBER WHEN YOU UPDATE THE MENU
-     Example:
-     If you edit menu.css or header.js, change 7 to 8
-     Then students only need a normal refresh
+     MENU VERSION
+     Change this number whenever you update menu.css
+     or header.js, so students get the new menu
+     with a normal browser refresh.
      ======================================================= */
-  const MENU_VERSION = "8";
+  const MENU_VERSION = "7";
 
-  /* Load shared menu CSS */
+  /* =======================================================
+     LOAD SHARED MENU CSS
+     This inserts menu.css into the <head>
+     ======================================================= */
   const css = document.createElement("link");
   css.rel = "stylesheet";
   css.href = "https://toniopaceict.github.io/level4_css/css/menu.css?v=" + MENU_VERSION;
   document.head.appendChild(css);
 
-  /* Load shared header/menu JS */
-  const headerScript = document.createElement("script");
-  headerScript.src = "https://toniopaceict.github.io/level4_css/js/header.js?v=" + MENU_VERSION;
-  document.body.appendChild(headerScript);
+  /* =======================================================
+     LOAD SHARED HEADER JS
+     Wait until the page is ready, then load header.js
+     This is important because header.js needs:
+     <div id="header-placeholder"></div>
+     to already exist in the page
+     ======================================================= */
+  function loadHeaderScript() {
+    const headerScript = document.createElement("script");
+    headerScript.src = "https://toniopaceict.github.io/level4_css/js/header.js?v=" + MENU_VERSION;
+    document.body.appendChild(headerScript);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", loadHeaderScript);
+  } else {
+    loadHeaderScript();
+  }
 })();
